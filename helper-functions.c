34504f51ee9f6d8f08b934f6ba2aabe31a26c53b
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-06 22:51:56 +0100

    Prototyped functions and implemented zPartOfSet

--- helper-functions.c
+++ helper-functions.c
@@ -1,1 +1,20 @@
 #include"helper-functions.h"
+
+bool zPartOfSet(double complex c, int iterations){
+    double complex z = 0;
+    double complex previous[iterations];
+    bool partOfSet = 0;
+    for(size_t i = 0; i < iterations && !partOfSet; i++){
+        z = (z * z) + c;
+        if(cabs(z)>6000000){
+            break;
+        }
+        for(size_t j = i; j > 0; j--){
+            if(z == previous[j]){
+                partOfSet = 1;
+            }
+        }
+        previous[i] = z;
+    }
+    return partOfSet;
+}
--- helper-functions.h
+++ helper-functions.h
@@ -3,8 +3,8 @@
 #include<stdbool.h>
 #include<stdio.h>
 
-static bool zPartOfSet(double complex c);
+static bool zPartOfSet(double complex c, int iterations);
 
 static double complex screenToComplex(int w, int h, int x, int y);
 
-bool PartOfSet(int w, int h, int x, int y);
+bool PartOfSet(int w, int h, int x, int y, int iterations);
