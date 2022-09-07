ac5893a74ed0606bebebfb480578650471197025
Frederick Watkins <freddiewatkins6@gmail.com>
2022-09-07 22:47:08 +0100

    Imported POSIX multithreading library

--- sdl-functions.h
+++ sdl-functions.h
@@ -1,6 +1,7 @@
 #include<stdbool.h>
 #include<SDL2/SDL.h>
 #include<SDL2/SDL_image.h>
+#include<pthread.h>
 #include"helper-functions.h"
 
 void sdlStartup(SDL_Window** win, SDL_Renderer** rend, int w, int h);
